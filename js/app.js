3bd350b37cba0113ce00b36156d211207fb05d2f
Jesse Phillips <mr.jesse.phillips@gmail.com>
2014-06-17 23:30:14 -0400

    small change to formatting

--- js/app.js
+++ js/app.js
@@ -1,20 +1,24 @@
 $(document).ready(function(){
 	
-	// fizz buzz loop thru 100. do alerts for now.
+	fizzBuzz();
+});
+
+function fizzBuzz(){
+		// fizz buzz loop thru 100 and do fizz buzz.
 	var nuNode;
 
 	for (var i = 1; i <= 100; i++) {
 		if ( (i % 5) == 0 && (i % 3) == 0) {
 			//fizz buzz
-			nuNode = $('<p>'+i+'/3 = '+i/3+' and '+i+'/5 = '+i/5+'fizz-buzz</p>');
+			nuNode = $('<p>FIZZ-BUZZ! ('+i+'/3 = '+i/3+' and '+i+'/5 = '+i/5+')</p>');
 		} else if ( (i % 3) == 0 ) {
-			nuNode = $('<p>'+i+'/3 = '+i/3+'fizz</p>');
+			nuNode = $('<p>fizz ('+i+'/3 = '+i/3+')</p>');
 		} else if ( (i % 5) == 0 ) {
-			nuNode = $('<p>'+i+'/5 = '+i/5+'buzz</p>');
+			nuNode = $('<p>buzz ('+i+'/5 = '+i/5+')</p>');
 		} else {
 			nuNode = $('<p>'+i+'</p>');
 		}
 
 		$(document).find('body').append(nuNode);
 	}
-});
+}
